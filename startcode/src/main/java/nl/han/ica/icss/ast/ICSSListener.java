d6173e1a1b45878e14db29361cd12e221b6686b2
jordy Eulink <jordy.eulink31@gmail.com>
2026-03-17 09:00:01 +0100

    Code running

---
+++ startcode/src/main/java/nl/han/ica/icss/ast/ICSSListener.java
@@ -0,0 +1,20 @@
+package nl.han.ica.icss.ast;// Generated from C:/Users/jordy/Schoolwerk-Han-HBO-ICT/25-26/Verdiepend semester/APP/Compiler opdarcht/icss2022-sep/startcode/src/main/antlr4/nl/han/ica/icss/parser/ICSS.g4 by ANTLR 4.13.2
+import nl.han.ica.icss.parser.ICSSParser;
+import org.antlr.v4.runtime.tree.ParseTreeListener;
+
+/**
+ * This interface defines a complete listener for a parse tree produced by
+ * {@link ICSSParser}.
+ */
+public interface ICSSListener extends ParseTreeListener {
+	/**
+	 * Enter a parse tree produced by {@link ICSSParser#stylesheet}.
+	 * @param ctx the parse tree
+	 */
+	void enterStylesheet(ICSSParser.StylesheetContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link ICSSParser#stylesheet}.
+	 * @param ctx the parse tree
+	 */
+	void exitStylesheet(ICSSParser.StylesheetContext ctx);
+}
